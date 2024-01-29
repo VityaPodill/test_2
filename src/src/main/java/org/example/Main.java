4519a5bbabf4b62bf23e9dd3070c9e7dcd234dbb
victor <podill1122333@google.com>
2024-01-29 11:44:22 +0200

    Implement diff method

--- src/src/main/java/org/example/Main.java
+++ src/src/main/java/org/example/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         int a=5;
         int b=7;
-        System.out.println(sum(a,b));
+        System.out.println(diff(a,b));
     }
-    private static int sum(int a,int b){
-        return a+b;
+    private static int diff(int a,int b){
+        return a-b;
     }
 }
