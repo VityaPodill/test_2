f5c8f477bee349ecd4b3e9e7bf36f0a234f88b30
victor <podill1122333@google.com>
2024-01-29 14:06:11 +0200

    Implement modulDivide (%) method

--- src/src/main/java/org/example/Main.java
+++ src/src/main/java/org/example/Main.java
@@ -2,23 +2,32 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int a=5;
-        int b=7;
-        System.out.println(diff(a,b));
-        System.out.println(sum(a,b));
-        System.out.println(mult(a,b));
-        System.out.println(div(a,b));
+        int a = 15;
+        int b = 7;
+        System.out.println(diff(a, b));
+        System.out.println(sum(a, b));
+        System.out.println(mult(a, b));
+        System.out.println(div(a, b));
+        System.out.println(mod(a, b));
     }
-    private static int sum(int a,int b){
-        return a+b;
+
+    private static int sum(int a, int b) {
+        return a + b;
     }
-    private static int diff(int a,int b){
-        return a-b;
+
+    private static int diff(int a, int b) {
+        return a - b;
     }
-    private static int mult(int a,int b){
-        return a*b;
+
+    private static int mult(int a, int b) {
+        return a * b;
     }
-    private static int div(int a,int b){
-        return a/b;
+
+    private static int div(int a, int b) {
+        return a / b;
+    }
+
+    private static int mod(int a, int b) {
+        return a % b;
     }
 }
