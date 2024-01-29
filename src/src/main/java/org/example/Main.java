4ec34684e71cf84b36c4aab3e5e233b40ee5c6f5
victor <podill1122333@google.com>
2024-01-29 11:42:44 +0200

    Implement sum method

---
+++ src/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int a=5;
+        int b=7;
+        System.out.println(sum(a,b));
+    }
+    private static int sum(int a,int b){
+        return a+b;
+    }
+}
