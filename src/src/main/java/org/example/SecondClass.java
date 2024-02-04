3b3607f581a8091c81eff6dec2a1422bea35dcd5
victor <podill1122333@google.com>
2024-02-04 19:01:57 +0200

    Create 'main' in SecondClass.

--- src/src/main/java/org/example/SecondClass.java
+++ src/src/main/java/org/example/SecondClass.java
@@ -2,10 +2,17 @@ package org.example;
 
 public class SecondClass {
   int a;
-  public SecondClass(int a){
-    this.a=a;
+
+  public SecondClass(int a) {
+    this.a = a;
   }
-  public int oneMethod(int a){
-    return a*a;
+
+  public int oneMethod(int a) {
+    return a * a;
+  }
+
+  public static void main(String[] args) {
+    SecondClass object = new SecondClass(15);
+    System.out.println(object.oneMethod(object.a));
   }
 }
