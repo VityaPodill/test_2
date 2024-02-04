4148fdd3f2d5ddff4ce6dfd880ea7701a7f99152
victor <podill1122333@google.com>
2024-02-04 20:56:38 +0200

    Many 'System.out' was added.

--- src/src/main/java/org/example/SecondClass.java
+++ src/src/main/java/org/example/SecondClass.java
@@ -16,10 +16,20 @@ public class SecondClass {
   }
 
   public static void main(String[] args) {
-    
+
     SecondClass object = new SecondClass(15);
-    
+
     System.out.println(object.oneMethod(object.a));
+    System.out.println(object.twoMethod(object.a));
+    System.out.println(object.twoMethod(object.a));
+    System.out.println(object.twoMethod(object.a));
+    System.out.println(object.twoMethod(object.a));
+    System.out.println(object.twoMethod(object.a));
+    System.out.println(object.twoMethod(object.a));
+    System.out.println(object.twoMethod(object.a));
+    System.out.println(object.twoMethod(object.a));
+    System.out.println(object.twoMethod(object.a));
+    System.out.println(object.twoMethod(object.a));
     System.out.println(object.twoMethod(object.a));
   }
 }
