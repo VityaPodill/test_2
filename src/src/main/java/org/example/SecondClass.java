ac3de0c1b0b44f0dd99ca7d5b0e429f16fe25493
victor <podill1122333@google.com>
2024-02-04 19:22:44 +0200

    Create 'twoMethod' in SecondClass.

--- src/src/main/java/org/example/SecondClass.java
+++ src/src/main/java/org/example/SecondClass.java
@@ -11,8 +11,13 @@ public class SecondClass {
     return a * a;
   }
 
+  private int twoMethod(int a) {
+    return a + 100;
+  }
+
   public static void main(String[] args) {
     SecondClass object = new SecondClass(15);
     System.out.println(object.oneMethod(object.a));
+    System.out.println(object.twoMethod(object.a));
   }
 }
