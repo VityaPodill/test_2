75c7a1bbfde9b6ecb01b614c96a0a45defb378ac
victor <podill1122333@google.com>
2024-02-05 22:33:11 +0200

    IDE---add 'Class_3'.

---
+++ src/src/main/java/org/example/Class_3.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Class_3 {
+    public static void main(String[] args) {
+        int a = 17, b = 24, c;
+        c = (int) Math.pow(b, 3);
+        System.out.println(a * b);
+
+        System.out.println(c);
+    }
+}
