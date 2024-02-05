fd9514c8261cb9b4e9dd97724cf1216c9d8f4d43
victor <podill1122333@google.com>
2024-02-05 11:36:07 +0200

    IDE---add Math.max v2.0

--- src/src/main/java/org/example/SecondClass.java
+++ src/src/main/java/org/example/SecondClass.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class SecondClass {
   int a;
-
+  int b = 7;
   public SecondClass(int a) {
     this.a = a;
   }
@@ -15,11 +15,14 @@ public class SecondClass {
     return a + 100;
   }
 
+  private int threeMethod(int a, int b) {
+    return Math.max(a, b);
+  }
+
   public static void main(String[] args) {
     SecondClass object = new SecondClass(15);
     System.out.println(object.oneMethod(object.a));
     System.out.println(object.twoMethod(object.a));
-    System.out.println(object.twoMethod(object.a));
-    System.out.println(object.twoMethod(object.a));
+    System.out.println(object.threeMethod(object.a, object.b));
   }
 }
