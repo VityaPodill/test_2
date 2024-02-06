7ea31ab246dec6fdbbad659710747e837ac6904e
victor <podill1122333@google.com>
2024-02-06 16:30:48 +0200

    ide change  Class-4

--- src/src/main/java/org/example/Class_4.java
+++ src/src/main/java/org/example/Class_4.java
@@ -1,20 +1,7 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Class_4 {
     public static void main(String[] args) {
-        String c = "1234567";
-        char[] a = new char[c.length()];
-        for (int i = 0; i < c.length(); i++) {
-            a[i] = c.charAt(i);
-        }
-        List<Character> l = new ArrayList<>();
-        for (int i = c.length() - 1; i >= 0; i--) {
-
-            l.add(a[i]);
-        }
-        System.out.println(l);
+        System.out.println(777);
     }
 }
