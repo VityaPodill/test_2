0e8beb163c0e137a6f321c4007b1205b5a55abcf
VityaPodill <119930971+VityaPodill@users.noreply.github.com>
2024-02-10 22:12:45 +0200

    GIT 'add abs.new'

--- src/src/main/java/org/example/Class_4.java
+++ src/src/main/java/org/example/Class_4.java
@@ -2,17 +2,9 @@ package org.example;
 
 public class Class_4 {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(22);
-        System.out.println(333);
-        System.out.println(4444);
-        System.out.println(55555);
-        System.out.println(777);
-        System.out.println(777);
-
-        System.out.println(888);
-        System.out.println(888);
-        System.out.println(888);
-        System.out.println(888);
+        int a = 17, b = 24, c;
+        c = (int) Math.pow(b, 3);
+        System.out.println(a * b);
+        System.out.println(c);
     }
 }
