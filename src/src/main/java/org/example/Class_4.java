38478ca2eb50faecb8af1e39ecc5e9fb38478c21
victor <podill1122333@google.com>
2024-02-10 14:11:37 +0200

    ide add ====

--- src/src/main/java/org/example/Class_4.java
+++ src/src/main/java/org/example/Class_4.java
@@ -9,5 +9,7 @@ public class Class_4 {
         System.out.println(55555);
         System.out.println(777);
         System.out.println("=====");
+        System.out.println("=====");
+        System.out.println("=====");
     }
 }
