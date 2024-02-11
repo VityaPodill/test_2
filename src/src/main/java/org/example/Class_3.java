31d2b1022d1a71cc8872f090bc0121f6d0c25179
victor <podill1122333@google.com>
2024-02-11 18:28:08 +0200

    ide 'del 888 in Class_3'

--- src/src/main/java/org/example/Class_3.java
+++ src/src/main/java/org/example/Class_3.java
@@ -8,10 +8,5 @@ public class Class_3 {
         System.out.println(c);
         System.out.println(55555);
         System.out.println(55555);
-
-        System.out.println(888);
-        System.out.println(888);
-        System.out.println(888);
-        System.out.println(888);
     }
 }
