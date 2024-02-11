613615e8139f989d6b9c6fff1a7c5c97cb5b0b57
VityaPodill <119930971+VityaPodill@users.noreply.github.com>
2024-02-11 09:50:37 +0200

    GIT 'del all except 888'

--- src/src/main/java/org/example/Class_4.java
+++ src/src/main/java/org/example/Class_4.java
@@ -2,20 +2,6 @@ package org.example;
 
 public class Class_4 {
     public static void main(String[] args) {
-        int a = 17, b = 24, c;
-        c = (int) Math.pow(a, 3);
-        System.out.println(a * b);
-        System.out.println(c);
-        System.out.println(1);
-        System.out.println(22);
-        System.out.println(333);
-        System.out.println(4444);
-        System.out.println(55555);
-        System.out.println(55555);
-        System.out.println(55555);
-        System.out.println(777);
-        System.out.println(777);
-
         System.out.println(888);
         System.out.println(888);
         System.out.println(888);
