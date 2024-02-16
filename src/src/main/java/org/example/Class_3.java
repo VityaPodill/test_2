7077247fe745235acc8d444358cd38963c077f54
VityaPodill <119930971+VityaPodill@users.noreply.github.com>
2024-02-16 22:01:37 +0200

    GIT del almost all

--- src/src/main/java/org/example/Class_3.java
+++ src/src/main/java/org/example/Class_3.java
@@ -4,15 +4,5 @@ public class Class_3 {
     public static void main(String[] args) {
         int a = 17, b = 24, c;
         c = (int) Math.pow(b, 3);
-        System.out.println(a * b);
-        System.out.println(a * b);
-        System.out.println(a * b);
-        System.out.println(c);
-        System.out.println(55555);
-        System.out.println(55555);
-        System.out.println(7);
-        System.out.println(7);
-        System.out.println(7);
-        System.out.println(7);
     }
 }
