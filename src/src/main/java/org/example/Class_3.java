bb17b83c907b851a14486c0a2ac4232d6c164272
victor <podill1122333@google.com>
2024-02-16 22:12:25 +0200

    ide 'add 7 in Class_3'

--- src/src/main/java/org/example/Class_3.java
+++ src/src/main/java/org/example/Class_3.java
@@ -3,16 +3,7 @@ package org.example;
 public class Class_3 {
     public static void main(String[] args) {
         int a = 17, b = 24, c;
-        c = (int) Math.pow(b, 3);
-        System.out.println(a * b);
-        System.out.println(a * b);
+        c = (int) Math.pow(b, 3) + a;
         System.out.println(c);
-        System.out.println(55555);
-        System.out.println(7);
-        System.out.println(7);
-        System.out.println(7);
-        System.out.println(7);
-        System.out.println(7);
-        System.out.println(7);
     }
 }
